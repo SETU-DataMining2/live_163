c9764a9a9c633b73169d52a01c173a450d184c0d
kmurphy <kmurphy@wit.ie>
2023-03-31 12:00:24 +0100

    21/03

--- topics/21-Assignments/03-Texas_Inpatient_Discharge/files/my_lib.py
+++ topics/21-Assignments/03-Texas_Inpatient_Discharge/files/my_lib.py
@@ -11,8 +11,45 @@ sns.set_style("darkgrid")
 from IPython.display import display, Markdown
 pd.set_option('display.max_columns', None)  
 
-import glob, os
+import glob, os, sys
 
+DATASET = "Texas_Inpatient_Discharge"
+SPLIT_TRAINING = True
 DEBUG = False
 SEED = 42
+
+COLAB = 'google.colab' in sys.modules
+if COLAB:
+    ROOT = f"/content/gdrive/MyDrive/datasets/{DATASET.replace(' ','_')}/"
+else:
+    ROOT = "./"
+
+
+from zipfile import ZipFile
+import os
+
+def make_assignment(files=None, archive="my_assignment.zip"):
+    """Create assignment archive for uploading to Moodle. Optional parameter `files` can be a list of additional files to include."""
+    default_files = {"01-Import.ipynb", "02-EDA.ipynb", "03-Model.ipynb", "my_lib.py", "df_grading_pred.csv", "dashboard.py"}
+
+    if files is None:
+        files = default_files
+    else:
+        if type(files)==str: files = [files]
+        if type(files) in [list,tuple]: files = set(files)
+        files = files.union(default_files)
+
+    print(f"Creating archive: {archive}")
+    with ZipFile(f"{ROOT}/{archive},"w") as zip:
+        for f in files:
+            if os.path.isfile(f"{ROOT}/{f}"):
+                print(f"\t{f} - OK")
+                zip.write(f) 
+            else:
+                print(f"\t{f} - Skipped (missing file)")
+
+
+################################################################
+# Put your code below this block
+################################################################
 
